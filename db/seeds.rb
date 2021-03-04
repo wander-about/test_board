72beeb530fcf89bfc409d2d96478ca560652517f
tjyun79 <tjyun79@gmail.com>
2021-03-04 18:00:41 +0900

    디바이스젬 설치 및 시드 설정

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+user_1 = User.create(email: "1@1", password: "111111", password_confirmation: "111111")
+user_2 = User.create(email: "2@2", password: "111111", password_confirmation: "111111")
+user_3 = User.create(email: "3@3", password: "111111", password_confirmation: "111111")
+User.create(email: "4@4", password: "111111", password_confirmation: "111111")
+User.create(email: "5@5", password: "111111", password_confirmation: "111111")
+
+Post.destroy_all
+
+Post.create(title: "test 1번 제목", content: "test 1번 글 내용입니다.", user: user_1)
+Post.create(title: "test 2번 제목", content: "test 2번 글 내용입니다.", user: user_2)
+Post.create(title: "test 3번 제목", content: "test 3번 글 내용입니다.", user: user_3)
